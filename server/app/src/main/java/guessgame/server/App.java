d8d6d227147b4573cf117653c6dd04ee342c3268
Marc <marc98711@gmail.com>
2023-04-29 18:06:55 +0200

    server v1

--- server/app/src/main/java/guessgame/server/App.java
+++ server/app/src/main/java/guessgame/server/App.java
@@ -3,12 +3,27 @@
  */
 package guessgame.server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+
+    public static final int SERVER_PORT = 9999;
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        
+        try (ServerSocket serverSocket = new ServerSocket(SERVER_PORT);) {
+            System.out.println("Server is up an running...");
+            
+            while (true) {
+                new GameThread(socket.accept()).start();
+
+            }
+            
+        }
+        catch (IOException e) {
+            System.out.println("Server run into a IOException!");
+        }
     }
 }
